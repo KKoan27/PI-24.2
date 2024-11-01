9a5aed3bdc8b8517aadb587d628b90f96f2159fd
davi <davijose2003@gmail.com>
2024-11-01 17:15:16 -0300

    Side-Bar Departamentos

--- octo/src/Componentes/Header.jsx
+++ octo/src/Componentes/Header.jsx
@@ -1,26 +1,97 @@
-import Logo from "../Imagens/Logo.png"
-import "./CompCss/Header.css"
-import {Link, useLocation} from 'react-router-dom'
-function Cabeçalho(){
+import React, { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { Box, Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Divider } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import HomeIcon from '@mui/icons-material/Home';
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import InfoIcon from '@mui/icons-material/Info';
+import PermContactCalendarIcon from '@mui/icons-material/PermContactCalendar';
+import InboxIcon from '@mui/icons-material/Inbox';
+import Logo from '../Imagens/Logo.png';
+import './CompCss/Header.css';
 
-     const location = useLocation(); 
-    return(
-        <header className=" Cabecalho">
-             <img src={Logo} alt="OctoLogo"/>
+const Header = () => {
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+    const location = useLocation();
 
-            <nav id= "HeaderOp">
-                    <Link>Departamentos</Link>
-            
-                    <Link id={location.pathname ==="/" ? "active.link" : "home-link"} to = {"/"}> Home</Link>
+    const toggleDrawer = (open) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+        setIsDrawerOpen(open);
+    };
 
+    const drawerList = (
+        <Box
+            sx={{ width: 250 }}
+            role="presentation"
+            onClick={toggleDrawer(false)}
+            onKeyDown={toggleDrawer(false)}
+        >
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', padding: '0 16px' }}>
+                <ListItem disablePadding>
+                    <ListItemButton component={Link} to="/">
+                        <ListItemIcon>
+                            <PermIdentityIcon/>
+                        </ListItemIcon>
+                        <ListItemText primary="Minha conta" />
+                    </ListItemButton>
+                </ListItem>
+    
+                <ListItem disablePadding>
+                    <ListItemButton component={Link} to="/about">
+                        <ListItemIcon>
+                            <InfoIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="About" />
+                    </ListItemButton>
+                </ListItem>
+            </Box>
+    
+            <Divider />
+    
+            <List>
+                <ListItem disablePadding>
+                    <ListItemButton component={Link} to="/contact">
+                        <ListItemIcon>
+                            <PermContactCalendarIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Contact" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <InboxIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Inbox" />
+                    </ListItemButton>
+                </ListItem>
+            </List>
+        </Box>
+    );
+
+    return (
+        <header className="Cabecalho">
+            <img src={Logo} alt="OctoLogo" />
+
+            <nav id="HeaderOp">
+                <IconButton onClick={toggleDrawer(true)} aria-label="open drawer">
+                    <MenuIcon sx={{ color: 'white' }} />
+                    <span style={{ color: 'white', marginLeft: '8px' }}>Departamentos</span>
+                </IconButton>
+                <Link id={location.pathname === "/" ? "active-link" : "home-link"} to="/">Home</Link>
                 <Link id={location.pathname === "/PCs-gamer" ? "active-link" : "pc-gamer-link"} to="/PCs-gamer">PC's Gamer</Link>
                 <Link id={location.pathname === "/Monte-seu-PC" ? "active-link" : "monte-pc-link"} to="/Monte-seu-PC">Monte seu PC</Link>
                 <Link id={location.pathname === "/Atendimento" ? "active-link" : "atendimento-link"} to="/Atendimento">Atendimento</Link>
                 <Link id={location.pathname === "/Login" ? "active-link" : "login-link"} to="/Login">Login</Link>
             </nav>
 
+            <Drawer anchor="left" open={isDrawerOpen} onClose={toggleDrawer(false)}>
+                {drawerList}
+            </Drawer>
         </header>
-        
-    )
-}
-export default Cabeçalho
+    );
+};
+
+export default Header;
