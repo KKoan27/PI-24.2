415cf08a0ee1230c3097426125ea3e4c8cf2d40f
davi <davijose2003@gmail.com>
2024-11-10 22:24:39 -0300

    Revert "Merge branch 'main' into Davi"
    This reverts commit c7accd27949dde908c5592d4676243c6f210995a, reversing
    changes made to 11d261732280323cdd666aae426450343181887f.

---
+++ octo/src/Componentes/PAGINAS/Atendimento.jsx
@@ -0,0 +1,10 @@
+import React  from "react";
+
+export default function Atendimento(){
+    return(
+        <div className="AtendiClass">
+            <h1>Atende Carai</h1>
+        </div>
+
+    )
+}
