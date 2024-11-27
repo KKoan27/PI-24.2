80d2f6bff88363ff4229e033106c57b5c57a795e
Vipudding <98952502+Vipudding@users.noreply.github.com>
2024-11-27 14:23:42 -0300

    Primeiro commit da pagina de montagem de PC
    Commit da parte do menu de peças

--- octo/src/Componentes/PAGINAS/Monte-seu-PC.jsx
+++ octo/src/Componentes/PAGINAS/Monte-seu-PC.jsx
@@ -1,10 +1,45 @@
-import React  from "react";
+import React, { useState } from "react";
+import "../CompCss/MontePC.css";
 
-export default function MontePC(){
-    return(
-        <div className="MontPC">
-            <h1>Aqui sera as montagens de Computadores</h1>
-        </div>
+export default function MontePC() {
+  const [selected, setSelected] = useState(""); // Estado para armazenar o item selecionado
 
-    )
+  const items = [
+    "Processador",
+    "Placa Mãe",
+    "Memória",
+    "Placa de vídeo",
+    "SSD",
+    "HD",
+    "Cooler",
+    "Fonte",
+    "Gabinete",
+    "Fans",
+    "Sistema Operacional",
+    "Software",
+  ];
+
+  const handleSelection = (item) => {
+    setSelected(item); // Atualiza o estado com o botão selecionado
+  };
+
+  return (
+    <div className="Main-MontePC">
+      <aside className="sidebar">
+        <ul>
+          {items.map((item, index) => (
+            <li
+              key={index}
+              className={`sidebar-item ${
+                selected === item ? "selected" : "deselected"
+              }`}
+              onClick={() => handleSelection(item)} // Define o botão clicado como selecionado
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      </aside>
+    </div>
+  );
 }
